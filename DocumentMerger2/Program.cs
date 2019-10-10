31e94906a0877250a1bf5f1591a3085bcbacd9f2
averywald <averywald.dev@gmail.com>
2019-10-10 12:27:27 -0500

    keep changes

--- DocumentMerger2/Program.cs
+++ DocumentMerger2/Program.cs
@@ -1,9 +1,42 @@
 ﻿using System;
 using System.Collections;
 using System.Linq;
+using System.IO;
+
+// TO DO:
+// - exception handling when wrong input format
+// - handle files not existing
 
 namespace DocumentMerger2 {
     class Program {
+
+        // check if the file arguments exist
+        static void checkFile(string[] files) {
+
+            // init arraylist to hold non-existent files
+            ArrayList errors = new ArrayList();
+
+            foreach (string f in files) {
+                if (!File.Exists(f)) {
+                    // add to list of non-existent files
+                    errors.Add(f);
+                }
+            }
+
+            // if non-existent files
+            if (errors.Count > 0) {
+                Console.WriteLine($"\nError: file(s):");
+                // print each to console in error message
+                foreach (string f in errors) {
+                    Console.WriteLine($"{f}");
+                }
+                Console.WriteLine("does not exist");
+                Console.WriteLine("Make sure the file path is correct\n");
+                // terminate the program
+                Environment.Exit(0);
+            }
+
+        }
         static void Main(string[] args) {
 
             // check if arguments are provided
@@ -22,8 +55,12 @@ namespace DocumentMerger2 {
                     // init array excluding the output file argument
                     var inputs = (string[])args.Take(args.Length - 1).ToArray();
 
+                    // check existence of inputs
+                    checkFile(inputs);
+
                     // add new Doc object for each argument
                     foreach (string i in inputs) {
+                        // else add file to docs arraylist
                         docs.Add(new Doc(i));
                     }
 
@@ -36,6 +73,7 @@ namespace DocumentMerger2 {
                 }
         
             }
+
             else {
                 Console.WriteLine("Error: no arguments specified");
             }
